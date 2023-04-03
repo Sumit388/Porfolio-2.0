bbb74e49ecf18df6ea9987eebdf11de1dc24e1ea
Sumit Mishra <sumit.mishra@uniacco.com>
2023-04-03 11:33:01 +0530

    Refactor: Setup files.

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/apple/Desktop/Learning/Porfolio-2.0/src/app/page": [
+      "/Users/apple/Desktop/Learning/Porfolio-2.0/src/app/globals.css"
+    ]
+  },
+  "/Users/apple/Desktop/Learning/Porfolio-2.0/src/app/layout.tsx": [
+    "/Users/apple/Desktop/Learning/Porfolio-2.0/src/app/globals.css"
+  ]
+}
