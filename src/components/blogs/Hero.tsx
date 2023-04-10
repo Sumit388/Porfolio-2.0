694bd5802574915f182632ee41dfc97a62d692a1
Sumit Mishra <sumit.mishra@uniacco.com>
2023-04-11 00:34:55 +0530

    Feat:Added new header

--- src/components/blogs/Hero.tsx
+++ src/components/blogs/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='h-[60vh] w-screen mt-20 flex justify-center items-center'>
+    <div className='h-[64vh] w-screen mt-16 flex justify-center items-center'>
         <div className='h-[50vh] w-[98vw] shadow-lg rounded-2xl relative overflow-hidden'>
         <img src={'https://cdn.pixabay.com/photo/2017/08/08/00/30/nature-2609624_1280.jpg'} alt="" className="absolute top-0  w-[98vw] h-[50vh] left-0 object-cover"/>
         </div>
