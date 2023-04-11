fdcec5d92cb4dc35a91f830ccbcbf8dda7bf8ae0
Sumit Mishra <sumit.mishra@uniacco.com>
2023-04-11 19:42:50 +0530

    Feat: Added description.

--- src/components/portfolio/ProjectsDescription.tsx
+++ src/components/portfolio/ProjectsDescription.tsx
@@ -8,9 +8,13 @@ function ProjectsDescription() {
   return (
     <div className="overflow-hidden w-full h-[100%] flex flex-col justify-end items-center">
       <div className="p-4  text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quod
-        cumque expedita veniam praesentium perspiciatis aut non veritatis. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Amet, nam.
+        Each project represents
+        a unique challenge, and an opportunity to demonstrate my creativity,
+        technical skills, and dedication to excellence. I am driven by the
+        belief that software has the power to make a positive impact in people's
+        lives, and I am honored to be a part of that journey. So come, be
+        inspired, and let's change the world together, one line of code at a
+        time.
       </div>
       <Image src={bgImage} alt="" className="" />
     </div>
